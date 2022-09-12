0ca6952b9f1d84f8094090ab823d5469717c65fc
SirEmitR <emit3reyes@gmail.com>
2022-09-11 22:17:56 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
+
+const diaEvento = 'Sun Sep 15 2022 18:30:00';
+
+
+
+const getRemainTime = deadline =>{
+    let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000) / 1000 ,
+    remainSecons = ('0' + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+    remainHours = ('0' + Math.floor(remainTime /3600 % 24)).slice(-2),
+    remainDays = ('0'+ Math.floor(remainTime / (3600 * 24))).slice(-2);
+    return {
+        remainTime, 
+        remainSecons,
+        remainMinutes,
+        remainHours,
+        remainDays
+    }
+}
+
+
+const countDown = (deadline, dias, horas, min, seg) =>{
+    const timerUpdate = setInterval(() =>{
+        let t = getRemainTime(deadline);
+        dias.innerHTML = `${t.remainDays}`;
+        horas.innerHTML = `${t.remainHours}`;
+        min.innerHTML = `${t.remainMinutes}`;
+        seg.innerHTML = `${t.remainSecons}`;
+        if(t.remainTime <= 1){
+            clearInterval(timerUpdate);
+            dias.innerHTML = `00`;
+            horas.innerHTML = `00`;
+            min.innerHTML = `00`;
+            seg.innerHTML = `00`;
+        }
+    }, 1000);
+}
+
+countDown(diaEvento, document.getElementById("dia"), document.getElementById('hora'), document.getElementById('min'),document.getElementById('seg'));
+
 const colors = ["#FF3333", "#006838", "rgba(255,255,255, 1)"];
 
 const numBalls = 50;
@@ -73,4 +114,28 @@ for (let i = 0; i < numConf; i++) {
       easing: "ease-in-out"
     }
   );
+});
+
+
+//
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+   document.getElementById('enviando').style.display="block";
+   document.getElementById("enviar").style.display="none";
+   const serviceID = 'service_5u3w5gc';
+   const templateID = 'template_8nu1mg4';
+   console.log("enviando")
+   setTimeout(() =>{
+        emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            document.getElementById('enviando').style.display="none";
+            document.getElementById('enviado').style.display="block";
+        }, (err) => {
+            document.getElementById('enviando').style.display="none";
+            document.getElementById('error').style.display="block";
+        alert(JSON.stringify(err));
+        });
+   },2000)
+   console.log("Enviado")
 });
